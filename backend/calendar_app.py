13210b98517c8995b4ebe71d9c56e05b3b884ed0
Agnieszka Jakubowska <a.jakubowska.tester@gmail.com>
2024-03-12 12:55:04 +0100

    Pierwsza wersja endpointa getjson. Zwraca eventy przypisane do Agaty w pliku file.json.

--- backend/calendar_app.py
+++ backend/calendar_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -6,19 +7,42 @@ app = Flask(__name__)
 @app.route("/")
 def login():
     return render_template("welcome.html")
+
+
 @app.route("/homepage/<name>")
 def homepage(name):
-    return render_template("homepage.html",name=name)
+    return render_template("homepage.html", name=name)
 
-@app.route ("/homepage/newevent")
-def newevent():
+
+@app.route("/homepage/newevent")
+def new_event():
     return render_template("newevent.html")
 
-@app.route ("/homepage/myinvitations")
-def myinvitations():
-    return render_template ("myinvitations.html")
+
+@app.route("/homepage/myinvitations")
+def my_invitations():
+    return render_template("myinvitations.html")
+
+
+@app.route('/returnjson', methods=['GET'])
+def return_json():
+    if request.method == 'GET':
+        with open("file.json", "r") as file:
+            data = json.load(file)
+            my_events_ids = []
+            users = data["users"]
+            for user in users["user"]:
+                if user["name"] == "Agata":
+                    my_events_ids = user["events"]
+            events = data["events"]
+            my_events = []
+            for event in events["event"]:
+                for event_id in my_events_ids:
+                    if event_id == event["id"]:
+                        my_events.append(event)
+            return jsonify(my_events)
+
 
 if __name__ == "__main__":
-    #app.run(host='192.168.31.67')
-    app.run()
-
+    # app.run(host='192.168.31.67')
+    app.run(debug=True)
